0dcc9a5b9295934ef1c107c6b145c01d3dfec130
Tammer Saleh <tsaleh@gmail.com>
2011-07-16 13:14:30 -0700

    tweaks to image mv script

--- scripts/mv_images.rb
+++ scripts/mv_images.rb
@@ -1,9 +1,17 @@
 #!/usr/bin/env bundle exec ruby
 
-CSV.read("config/images.csv").each do |i,file,desc,source_url|
-  source_file = Dir["content/attachments/images/#{i}-original.*"].first
-  dest_file   = "content/attachments/images/#{file}/original.#{File.extname(source_file)}"
-  FileUtil.mkdir_p File.dirname(dest_file)
-  FileUtil.mv source_file, dest_file
+require 'csv'
+require 'fileutils'
+
+include FileUtils::Verbose
+
+CSV.read("config/images.csv", :headers => true).each do |row|
+  source_file_glob = "#{File.dirname(__FILE__)}/../content/attachments/images/#{row["id"]}-original.*"
+  source_file = Dir[source_file_glob].first
+  puts "Can't find original file in #{source_file_glob}" unless source_file
+  puts "Converting #{source_file}"
+  dest_file = "content/attachments/images/#{row["file"]}/original.#{File.extname(source_file)}"
+  mkdir_p File.dirname(dest_file)
+  mv source_file, dest_file
 end
 
