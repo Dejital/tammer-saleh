4d3a6f3ea563f88e70fe0bccf4467cc5f11ef577
Tammer Saleh <tsaleh@gmail.com>
2011-07-16 13:19:45 -0700

    WIP

--- scripts/mv_images.rb
+++ scripts/mv_images.rb
@@ -3,14 +3,20 @@
 require 'csv'
 require 'fileutils'
 
-include FileUtils::Verbose
+include FileUtils
 
 CSV.read("config/images.csv", :headers => true).each do |row|
   source_file_glob = "#{File.dirname(__FILE__)}/../content/attachments/images/#{row["id"]}-original.*"
   source_file = Dir[source_file_glob].first
+
   puts "Can't find original file in #{source_file_glob}" unless source_file
-  puts "Converting #{source_file}"
-  dest_file = "content/attachments/images/#{row["file"]}/original.#{File.extname(source_file)}"
+
+  file_without_ext = File.basename(row["file"], ".*")
+  ext = File.extname(source_file)
+  dest_file = "content/attachments/images/#{file_without_ext}/original.#{ext}"
+
+  puts dest_file
+
   mkdir_p File.dirname(dest_file)
   mv source_file, dest_file
 end
