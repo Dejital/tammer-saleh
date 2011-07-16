18417b6388f28890f03cea68119dff8d9f9d73c7
Tammer Saleh <tsaleh@gmail.com>
2011-07-16 13:42:58 -0700

    Modified script to work with all files

--- scripts/mv_images.rb
+++ scripts/mv_images.rb
@@ -6,18 +6,21 @@ require 'fileutils'
 include FileUtils
 
 CSV.read("config/images.csv", :headers => true).each do |row|
-  source_file_glob = "#{File.dirname(__FILE__)}/../content/attachments/images/#{row["id"]}-original.*"
-  source_file = Dir[source_file_glob].first
+  base_dir = "#{File.dirname(__FILE__)}/../content/attachments/images"
+  %w(original large medium thumb).each do |size|
+    source_file_glob = "#{base_dir}/#{row["id"]}-#{size}.*"
+    source_file = Dir[source_file_glob].first
 
-  puts "Can't find original file in #{source_file_glob}" unless source_file
+    puts "Can't find original file in #{source_file_glob}" unless source_file
 
-  file_without_ext = File.basename(row["file"], ".*")
-  ext = File.extname(source_file)
-  dest_file = "content/attachments/images/#{file_without_ext}/original#{ext}"
+    file_without_ext = File.basename(row["file"], ".*")
+    ext = File.extname(source_file)
+    dest_file = "#{base_dir}/#{file_without_ext}/#{size}#{ext}"
 
-  puts dest_file
+    puts dest_file
 
-  mkdir_p File.dirname(dest_file)
-  mv source_file, dest_file
+    mkdir_p File.dirname(dest_file)
+    mv source_file, dest_file
+  end
 end
 
